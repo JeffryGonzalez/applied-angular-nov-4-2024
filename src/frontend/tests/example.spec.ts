209957426cbe4ab65de14c44d5b8baaa43fe1a78
Jeffry <jeff@hypertheory.com>
2024-11-06 13:47:48 -0500

    tests fixed

--- src/frontend/tests/example.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Can Start Up', async ({page}) => {
-    await page.goto('http://localhost:4200');
-    await expect(page).toHaveTitle('Applied Angular');
-})
